965d902fe38910036c975aa61a2b7ee77c2adea6
Lucas Augustin <lucas.augustin@cwi.com.br>
2020-04-25 16:13:28 -0300

    improve input functionality

--- chat-app/src/ui/screens/home/home.screen.js
+++ chat-app/src/ui/screens/home/home.screen.js
@@ -7,6 +7,7 @@ export function HomeScreen() {
   const [messages, setMessages] = useState([]);
 
   const scrollTargetRef = useRef(null)
+  const messageInputRef = useRef(null)
 
   const handleChange = (e) => {
     setMessage(e.target.value)
@@ -17,11 +18,16 @@ export function HomeScreen() {
   }, [messages])
 
   const handleSubmit = () => {
+    messageInputRef.current.focus()
+
+    if (!message) return
+
     setMessages([...messages, {
       author: 'Lucas',
       content: message,
       id: Math.random()*100
-    }])
+    }]);
+    setMessage('');
   }
 
   return (  
@@ -36,7 +42,12 @@ export function HomeScreen() {
           <div ref={scrollTargetRef} />
         </div>
         <div className="home-bottom-wrapper">
-          <input className="home--text-input" onChange={handleChange} />
+          <input
+            className="home--text-input"
+            onChange={handleChange}
+            value={message}
+            ref={messageInputRef}
+          />
           <button className="home--submit-button" onClick={handleSubmit}> > </button>
         </div>
       </div>
